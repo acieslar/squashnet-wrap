4dfaf542ed6180eb0aaa56710076c1207bb13a38
Andrzej Cieslar <andrzej.cieslar@gmail.com>
2018-02-13 23:04:20 +0100

    Switch to request-promise

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const path = require("path");
-const request = require("request");
+const rp = require("request-promise");
 const cheerio = require("cheerio");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
 
-function loadWiki(callback) {
-	this.result = [];
-	this.count = 0;
-	this.num = 0;
-	var getTournamentPage = (num) => {
+function testFunc() {
+    return getTournamentsList();
+}
+
+async function getTournamentsList() {
+    var tournaments = [];
+    const r1 = await getTournamentsChunk(1);
+    const r2 = await getTournamentsChunk(2);
+    const r3 = await getTournamentsChunk(3);
+    tournaments = tournaments.concat(r1);
+    tournaments = tournaments.concat(r2);
+    tournaments = tournaments.concat(r3);
+    return tournaments;
+}
+
+function getTournamentsChunk(num) {
+	return new Promise((resolve, reject) => {
 		var url = "https://www.squashnet.fr/Src/index.php?ajax=1&bnAction=132359";
 		var options = {
 			url: url,
@@ -25,39 +37,36 @@ function loadWiki(callback) {
 				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
 				"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
 				"Referer": "https://www.squashnet.fr/Src/",
-				//"Accept-Encoding": "gzip, deflate, br",
 				"Accept-Language": "en-US,en;q=0.9",
 				"Cookie": "PHPSESSID=0ah7gv1dskdhvvq97292vmncq4; authchallenge=30eee841c94bcc19b56a9a1d5cd4aa04"
 			}
 		};
-		request
-			.post(options, (error, res, body) => {
+		rp(options)
+			.then((body) => {
 				json = JSON.parse(body);
+				var result = [];
 				json.rows.forEach((row, idx) => {
+					let id = row.id;
 					let tdate = row.cell[1];
 					let tlink = row.cell[4];
 					const $ = cheerio.load(tlink);
-					this.num++;
 					let title = $('a').text();
-					this.result.push({num: this.num, date: tdate, title: title});
+					result.push({id: id, date: tdate, title: title});
 				});
-				this.count++;
-				if (this.count==3) {
-					console.log(`Callback: ${this.result}`);
-					callback(this.result);
+				resolve(result);
+			}).catch((err) => {
+					console.log(err);
 				}
-			});
-	}
-	getTournamentPage(1);
-	getTournamentPage(2);
-	getTournamentPage(3);
+			)
+		});
 }
 
 app.get("/tournaments", (req, res) => {
-  loadWiki((body) => {
-		res.send(body);
-	});
-}); 
+  testFunc().then((tlist) => {
+		res.send(tlist);
+    })
+  }
+);
 
 app.get("/", function (req, res) {
    res.sendFile(path.join(__dirname, "build", "index.html"));
